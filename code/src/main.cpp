2c25a88dd4cd13d74ae9c6c67ece2c3524819b1b
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2018-01-02 11:46:03 +0100

    Repo cleaned

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,5 +1,3 @@
-#include <Arduino.h>
-
 #include "MAX9814.h"
 #include "Jewel.h"
 
@@ -9,32 +7,14 @@
 #define NUMBER_OF_PIXELS 7 // Number of pixels you are using
 
 Jewel jewel = Jewel(LED_PIN, NUMBER_OF_PIXELS);
-MAX9814 mic = MAX9814(MIC_PIN, 600);
+MAX9814 mic = MAX9814(MIC_PIN);
 
 void setup()
 {
+
 }
 
 void loop()
 {
-    mic.fft();
-
-    uint16_t peakFreq = mic.getPeakFrequency();
-    uint16_t highFreq = mic.getHighFrequency();
-    uint16_t mediumFreq = mic.getMediumFrequency();
-    uint16_t lowFreq = mic.getLowFrequency();
-
-    uint16_t red = map(highFreq, 0, 64, 255, 0);
-    uint16_t blue = map(lowFreq, 0, 64, 255, 0);
-    uint16_t green = map(mediumFreq, 0, 64, 255, 0);
-
-    if (red > blue)
-    {
-        jewel.setPixelsColor(red, 0, 0, 0);
-    }
-    else
-    {
-        jewel.setPixelsColor(0, 0, blue, 0);
-    }
 
 }
